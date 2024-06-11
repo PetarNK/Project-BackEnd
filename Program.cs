fd8854adc4f2a5705cd7ed22030693fedab0ca4d
PetarNK <89004582+PetarNK@users.noreply.github.com>
2024-06-11 14:49:47 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Backend.Services;
+
+namespace Backend
+{
+    class Program
+    {
+        //This is your app entry point
+        static void Main(string[] args)
+        {
+            var container = ConfigureContainer();
+
+            //Get your application menu class
+            var application = container.Resolve<ApplicationService>();
+
+            //Run your application
+            application.Run();
+        }
+
+        //You should configure DI container (Autofac) or other DI Framework
+        private static IContainer ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            //Here you should register Interfaces with their referent classes
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Services/ApplicationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Backend.Services
+{
+    public class ApplicationService
+    {
+        //Here you should create Menu which your Console application will show to user
+        //User should be able to choose between: 1. Movie star 2. Calculate Net salary 3. Exit
+
+        public ApplicationService()
+        {
+        }
+
+        public void Run()
+        {
+            var userChoice = DisplayMenu();
+        }
+
+        private int DisplayMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
